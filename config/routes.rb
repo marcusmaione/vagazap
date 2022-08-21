dd709671bd4c263a29c1e2c958942ce9bf3e3540
Marcus Maione <marcusmaione@hotmail.com>
2022-08-21 13:28:22 -0300

    added companies with devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   get "user_profile", to: "users/profiles#show", as: :user_profile
+  get "company_profile", to: "companies/profiles#show", as: :company_profile
 
   devise_for :users, controllers: {
     confirmations:      'users/confirmations',
@@ -12,4 +13,12 @@ Rails.application.routes.draw do
     sessions:           'users/sessions',
     unlocks:            'users/unlocks'
   }
+
+  devise_for :companies, controllers: {
+    confirmations:      'companies/confirmations',
+    passwords:          'companies/passwords',
+    registrations:      'companies/registrations',
+    sessions:           'companies/sessions',
+    unlocks:            'companies/unlocks'
+  }  
 end
