653342f1bcdd501ffd545f2211968d0e941eb53e
Marcus Maione <marcusmaione@hotmail.com>
2022-08-25 15:16:20 -0300

    signup page with nice looking form now

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable
+  
+  CITIES = ["Lisbon", "London", "New York", "Remote"].freeze
 end
