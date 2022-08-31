4b6d186dfb6e5c50bb30ab8df1db32a4fd9c4b4e
Marcus Maione <marcusmaione@hotmail.com>
2022-08-31 09:02:40 -0300

    added create jobs flow

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
   JOBPOSITIONS = ["Intern", "Analyst", "Manager", "Director"].freeze
+  JOBLEVELS = ["Junior", "Senior"].freeze
   belongs_to :company
+  validates :content, presence: true
 end
