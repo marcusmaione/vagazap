26da8592b043cfddc01ff40df720556acde635a6
Marcus Maione <marcusmaione@hotmail.com>
2022-09-11 18:12:45 -0300

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -209,7 +209,7 @@ end
     Company.create(name: "Company #{i+5}", sector: "Banking", city: "Lisbon",
                    email: "company#{i+5}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+5}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+5}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -217,7 +217,7 @@ end
     Company.create(name: "Company #{i+10}", sector: "Banking", city: "Lisbon",
                 email: "company#{i+10}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+10}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+10}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
 
@@ -235,7 +235,7 @@ end
     Company.create(name: "Company #{i+20}", sector: "Retail", city: "Lisbon",
                    email: "company#{i+20}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+20}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+20}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -243,7 +243,7 @@ end
     Company.create(name: "Company #{i+25}", sector: "Retail", city: "Lisbon",
                 email: "company#{i+25}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+25}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+25}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
 
@@ -261,7 +261,7 @@ end
     Company.create(name: "Company #{i+35}", sector: "Technology", city: "Lisbon",
                    email: "company#{i+35}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+35}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+35}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -269,7 +269,7 @@ end
     Company.create(name: "Company #{i+40}", sector: "Technology", city: "Lisbon",
                 email: "company#{i+40}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+40}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+40}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
 
@@ -287,7 +287,7 @@ end
     Company.create(name: "Company #{i+50}", sector: "Banking", city: "Remote",
                    email: "company#{i+50}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+50}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+50}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -295,7 +295,7 @@ end
     Company.create(name: "Company #{i+55}", sector: "Banking", city: "Remote",
                 email: "company#{i+55}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+55}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+55}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
 
@@ -313,7 +313,7 @@ end
     Company.create(name: "Company #{i+65}", sector: "Retail", city: "Remote",
                    email: "company#{i+65}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+65}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+65}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -321,7 +321,7 @@ end
     Company.create(name: "Company #{i+70}", sector: "Retail", city: "Remote",
                 email: "company#{i+70}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+70}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+70}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
 
@@ -339,7 +339,7 @@ end
     Company.create(name: "Company #{i+80}", sector: "Technology", city: "Remote",
                    email: "company#{i+80}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+80}", title: "Manager", level: "Junior", sector: "Sales",
+    Job.create(company_id: "#{i+80}", title: "Manager", level: "Junior", sector: "Sales",
                description: "A description.", education_requirement: "Master's")
 end
 
@@ -347,6 +347,6 @@ end
     Company.create(name: "Company #{i+85}", sector: "Technology", city: "Remote",
                 email: "company#{i+85}@gmail.com", password: "apapap", password_confirmation: "apapap")
     
-    Job.create(user_id: "#{i+85}", title: "Director", level: "Senior", sector: "Technology",
+    Job.create(company_id: "#{i+85}", title: "Director", level: "Senior", sector: "Technology",
                description: "A description.", education_requirement: "Doctorate")
 end
